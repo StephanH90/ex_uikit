9e4435f155bf90a18f2cb40dcb0e5c59fa7c8e93
Stephan Hug <stephan.hug@adfinis.com>
2025-06-20 16:00:55 +0200

    initial commit

---
+++ lib/ex_uikit.ex
@@ -0,0 +1,18 @@
+defmodule ExUikit do
+  @moduledoc """
+  Documentation for `ExUikit`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExUikit.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_uikit_test.exs
@@ -0,0 +1,8 @@
+defmodule ExUikitTest do
+  use ExUnit.Case
+  doctest ExUikit
+
+  test "greets the world" do
+    assert ExUikit.hello() == :world
+  end
+end
